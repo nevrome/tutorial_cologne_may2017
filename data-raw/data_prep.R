903d17632f62ceeede0f5b676c0875a832a36be3
nevrome <clemens@nevrome.de>
2017-05-14 13:41:20 +0200

    better example data

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,7 +1,7 @@
 # trench corners
 trench <- read.csv("data-raw/trench.txt")
 save(trench, file = "data/trench.rda")
-#save(trench, file = "../../expack/data/trench.rda")
+save(trench, file = "../../expack/data/trench.rda")
 
 # sherd positions
 sherds <- read.csv(
@@ -9,7 +9,7 @@ sherds <- read.csv(
   stringsAsFactors = FALSE
 )
 save(sherds, file = "data/sherds.rda")
-#save(sherds, file = "../../expack/data/sherds.rda")
+save(sherds, file = "../../expack/data/sherds.rda")
 
 # Nprofile picture
 Nprofile <- jpeg::readJPEG(
@@ -17,19 +17,19 @@ Nprofile <- jpeg::readJPEG(
   native = TRUE
 )
 save(Nprofile, file = "data/Nprofile.rda")
-#save(Nprofile, file = "../../expack/data/Nprofile.rda")
+save(Nprofile, file = "../../expack/data/Nprofile.rda")
 
 # Nprofile_corners 
 Nprofile_corners <- read.csv(
   "data-raw/Nprofile_corners.txt"
 )
 save(Nprofile_corners, file = "data/Nprofile_corners.rda")
-#save(Nprofile_corners, file = "../../expack/data/Nprofile_corners.rda")
+save(Nprofile_corners, file = "../../expack/data/Nprofile_corners.rda")
 
 # Nivellement measurements
-nivellement <- read.csv(
-  "data-raw/nivellement.txt"
-)
-save(nivellement, file = "data/nivellement.rda")
-#save(nivellement, file = "../../expack/data/nivellement.rda")
-
+nivellement1 <- read.csv("data-raw/nivellement1.txt")
+save(nivellement1, file = "data/nivellement1.rda")
+save(nivellement1, file = "../../expack/data/nivellement1.rda")
+nivellement2 <- read.csv("data-raw/nivellement2.txt")
+save(nivellement2, file = "data/nivellement2.rda")
+save(nivellement2, file = "../../expack/data/nivellement2.rda")
